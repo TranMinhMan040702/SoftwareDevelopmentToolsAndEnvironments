f6a1b5a0745d438a7066106c4fce0a9fae5b58b3
NMS1010 <nguyenminhson102002@gmail.com>
2023-02-13 22:51:19 +0700

    test pull request

--- ProjectWeek1/src/main/java/com/msk/controller/Calculate.java
+++ ProjectWeek1/src/main/java/com/msk/controller/Calculate.java
@@ -2,23 +2,22 @@ package com.msk.controller;
 
 public class Calculate {
     public static float getResult(String operator, float firstNumber, float secondNumber){
-        float result = 0;
-
+        float res = 0;
 
         switch (operator) {
             case "+":
-                result = firstNumber + secondNumber;
+                res = firstNumber + secondNumber;
                 break;
             case "-":
-                result = firstNumber - secondNumber;
+                res = firstNumber - secondNumber;
                 break;
             case "*":
-                result = firstNumber * secondNumber;
+                res = firstNumber * secondNumber;
                 break;
             case "/":
-                result = firstNumber / secondNumber;
+                res = firstNumber / secondNumber;
         }
 
-        return result;
+        return res;
     }
 }
